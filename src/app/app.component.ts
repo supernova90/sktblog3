84d586556e8fa2c333938a84f379c353544197e8
supernova90 <majdiabbasjob@gmail.com>
2023-06-15 17:21:24 +0200

    second edition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-
 })
 export class AppComponent {
+  articleForm = new FormGroup({
+    title: new FormControl(''),
+    content: new FormControl('')
+  });
+
   title = 'sktblog3';
-  articles : Array<any>
+  articles: Array<any> = [];
 
   constructor() {
-    this.articles= [
-      {title: 'myblog1', content: 'hello1', comments:[{author: 'user1', content: 'nice'}]},
-      {title: 'myblog2', content: 'hello2', comments:[{author: 'user2', content: 'nice'}]},
-      {title: 'myblog3', content: 'hello3', comments:[{author: 'user3', content: 'nice'}]},
-      {title: 'myblog4', content: 'hello4', comments:[{author: 'user4', content: 'nice'}]}
-    ]
+ 
 
+    this.articles = [
+      { title: 'myblog1', content: 'hello1', comments: [{ author: 'user1', content: 'nice' }] },
+      { title: 'myblog2', content: 'hello2', comments: [{ author: 'user2', content: 'nice' }] },
+      { title: 'myblog3', content: 'hello3', comments: [{ author: 'user3', content: 'nice' }] },
+      { title: 'myblog4', content: 'hello4', comments: [{ author: 'user4', content: 'nice' }] }
+    ];
   }
+    addArticle(){
+      this.articles.push({
+        title: this.articleForm.value.title,
+        content: this.articleForm.value.content,
+        comments: []
+      });
+
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { CommentaireComponent } from './commentaire/commentaire.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { CommentaireComponent } from './commentaire/commentaire.component';
     CommentaireComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
